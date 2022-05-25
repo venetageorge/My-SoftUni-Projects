f48374874cec910b87f1c5b1ce7db4aff5010484
venetageorge <veneta.george@yahoo.com>
2022-05-25 11:50:52 +0300

    Adding my projects and console tasks files for Programming Fundamentals with JS.

--- Programming-fundamentals/JS01 (5) - Basic Syntax, Conditional Statements and Loops/JSD/04. reverseString.js
+++
@@ -1,8 +0,0 @@
-function reverseString(str) {
-    let text = '';
-    text = str.split('').reverse().join('');
-    console.log(text);
-}
-reverseString("Hello");
-reverseString("SoftUni");
-reverseString("1234");
