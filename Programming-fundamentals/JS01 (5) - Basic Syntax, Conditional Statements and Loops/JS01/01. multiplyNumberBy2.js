c66dbd564628afed3d87e0f76b564453d2e7aa81
veneta.george <veneta.george@yahoo.com>
2022-05-25 11:39:53 +0300

    Delete Programming-fundamentals/JS01 (5) - Basic Syntax, Conditional Statements and Loops directory

--- Programming-fundamentals/JS01 (5) - Basic Syntax, Conditional Statements and Loops/JS01/01. multiplyNumberBy2.js
+++
@@ -1,6 +0,0 @@
-function multiplyNumberBy2(num) {
-    console.log(num * 2);
-}
-multiplyNumberBy2(2);
-multiplyNumberBy2(5);
-multiplyNumberBy2(20);
--- Programming-fundamentals/JS01 (5) - Basic Syntax, Conditional Statements and Loops/JS01/09. numbersFromNTo1.js
+++
@@ -1,8 +0,0 @@
-function numbersFromNTo1(num) {
-
-    for (let i = num; i >= 1; i--) {
-        console.log(i);
-    }
-}
-numbersFromNTo1(5);
-numbersFromNTo1(3);
