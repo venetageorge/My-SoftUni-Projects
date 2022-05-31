219107fb3ef0ac7ee06e53394ea086e55e948a3f
veneta.george <veneta.george@yahoo.com>
2022-05-31 10:52:22 +0300

    Delete 07. triangleArea.js

--- Programming-fundament/JS02 (8) - Data Types and Variables/JSD/07. triangleArea.js
+++
@@ -1,7 +0,0 @@
-function triangleArea(side1, side2, side3) {
-    let side = (side1 + side2 + side3) / 2;
-    let area = Math.sqrt(side * ((side - side1) * (side - side2) * (side - side3)));
-    console.log(area);
-}
-triangleArea(2, 3.5, 4);
-triangleArea(3, 5.5, 4);
