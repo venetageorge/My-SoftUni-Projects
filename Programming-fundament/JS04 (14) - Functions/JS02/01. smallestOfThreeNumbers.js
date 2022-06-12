343558df8d8cabc41e3b9b245f921617928d37dd
venetageorge <veneta.george@yahoo.com>
2022-06-12 14:46:20 +0300

    Adding my projects and console tasks files for Programming Fundamentals with JS.

---
+++ Programming-fundament/JS04 (14) - Functions/JS02/01. smallestOfThreeNumbers.js
@@ -0,0 +1,8 @@
+function smallestOfThreeNumbers(num1, num2, num3) {
+    let smallestNum = Math.min(num1, num2, num3);
+    console.log(smallestNum);
+}
+smallestOfThreeNumbers(2, 5, 3);
+smallestOfThreeNumbers(600, 342, 123);
+smallestOfThreeNumbers(25, 21, 4);
+smallestOfThreeNumbers(2, 2, 2);
