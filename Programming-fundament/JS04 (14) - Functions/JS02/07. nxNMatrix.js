8202bbe69d8418444eaf45647e070914f3173d29
venetageorge <veneta.george@yahoo.com>
2022-06-13 15:52:51 +0300

    Adding my projects and console tasks files for Programming Fundamentals with JS.

---
+++ Programming-fundament/JS04 (14) - Functions/JS02/07. nxNMatrix.js
@@ -0,0 +1,16 @@
+function nxNMatrix(num) {
+    let matrix = [];
+
+    for (let i = 0; i < num; i++) {
+        let row = [];
+        for (let j = 0; j < num; j++) {
+            row.push(num);
+
+        }
+        matrix.push(row.join(' '));
+    }
+    console.log(matrix.join('\n'));
+}
+nxNMatrix(3);
+nxNMatrix(7);
+nxNMatrix(2);
