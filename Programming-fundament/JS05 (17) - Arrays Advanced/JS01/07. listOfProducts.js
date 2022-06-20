5b98f1eb64688b253691a82824ffe98e4852b10a
venetageorge <veneta.george@yahoo.com>
2022-06-20 13:30:17 +0300

    Adding my projects and console tasks files for Programming Fundamentals with JS.

---
+++ Programming-fundament/JS05 (17) - Arrays Advanced/JS01/07. listOfProducts.js
@@ -0,0 +1,9 @@
+function listOfProducts(input) {
+    let sorted = input.sort();
+
+    for (let i = 0; i < sorted.length; i++) {
+        console.log(`${i + 1}.${sorted[i]}`);
+    }
+}
+listOfProducts(['Potatoes', 'Tomatoes', 'Onions', 'Apples']);
+listOfProducts(['Watermelon', 'Banana', 'Apples']);
