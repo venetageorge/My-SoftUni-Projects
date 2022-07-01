b416d03ba6bc69ef713e7dd41cd5b2a14bf11282
venetageorge <veneta.george@yahoo.com>
2022-07-01 18:13:46 +0300

    Adding my projects and console tasks files for Programming Fundamentals with JS.

---
+++ Programming-fundament/JS06 (21) - Objects and Classes/JS01/02. city.js
@@ -0,0 +1,19 @@
+function city(cityInfo) {
+    for (let key of Object.keys(cityInfo)) {
+        console.log(`${key} -> ${cityInfo[key]}`);
+    }
+}
+city({
+    name: "Sofia",
+    area: 492,
+    population: 1238438,
+    country: "Bulgaria",
+    postCode: "1000"
+});
+city({
+    name: "Plovdiv",
+    area: 389,
+    population: 1162358,
+    country: "Bulgaria",
+    postCode: "4000"
+});
