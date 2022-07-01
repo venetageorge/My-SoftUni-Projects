26e09ee6a1b6092cffbdf1232e52065d894bc4e9
venetageorge <veneta.george@yahoo.com>
2022-07-01 18:12:37 +0300

    Adding my projects and console tasks files for Programming Fundamentals with JS.

---
+++ Programming-fundament/JS06 (21) - Objects and Classes/JS01/01. personInfo.js
@@ -0,0 +1,10 @@
+function personInfo(firstName, lastName, age) {
+    let myObj = {};
+
+    myObj.firstName = firstName;
+    myObj.lastName = lastName;
+    myObj.age = age; 
+    return myObj; 
+}
+console.log("Peter", "Pan", "20");
+console.log("George", "Smith", "18");
