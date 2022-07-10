30c71a13b2bd3d4afa81540ca11236efc4654fc5
venetageorge <veneta.george@yahoo.com>
2022-07-10 09:49:48 +0300

    Adding my projects and console tasks files for Programming Fundamentals with JS.

---
+++ Programming-fundament/JS07 (24) - Associative Arrays/JS02/01. wordTracker.js
@@ -0,0 +1,29 @@
+function wordTracker(input) {
+    let result = new Map();
+    let words = input.shift().split(' ');
+
+    for (let word of words) {
+        result.set(word, 0);
+    }
+
+    for (let word of input) {
+        if (result.has(word)) {
+            let oldValue = result.get(word);
+            result.set(word, oldValue + 1);
+        }
+    }
+
+    let sortResult = Array.from(result.entries()).sort(([keyA, valueA], [keyB, valueB]) => {
+        return valueB - valueA;
+    });
+
+    for (let [key, value] of sortResult) {
+        console.log(key, '-', value);
+    }
+}
+wordTracker([
+    'this sentence', 'In', 'this', 'sentence', 'you', 'have', 'to', 'count', 'the', 'occurrences', 'of', 'the', 'words', 'this', 'and', 'sentence', 'because', 'this', 'is', 'your', 'task'
+    ]);
+// wordTracker([
+//     'is the', 
+//     'first', 'sentence', 'Here', 'is', 'another', 'the', 'And', 'finally', 'the', 'the', 'sentence']);
