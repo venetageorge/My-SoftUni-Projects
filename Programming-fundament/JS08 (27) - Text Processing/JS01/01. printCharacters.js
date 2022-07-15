de28820cb022c38bac019d4c1b845ca84146135e
venetageorge <veneta.george@yahoo.com>
2022-07-15 12:29:18 +0300

    Adding my projects and console tasks files for Programming Fundamentals with JS.

---
+++ Programming-fundament/JS08 (27) - Text Processing/JS01/01. printCharacters.js
@@ -0,0 +1,12 @@
+function printCharacters(input) {
+
+    // for (let i = 0; i < input.length; i++) {
+    //     console.log(input[i]);
+    // }
+
+    for (let ch of input) {
+        console.log(ch);
+    }
+}
+printCharacters('AWord');
+printCharacters('Sentence');
