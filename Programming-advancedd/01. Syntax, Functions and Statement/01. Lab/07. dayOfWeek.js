122de8fdf17aa5fca41a81943c493e871bbfa0e6
venetageorge <veneta.george@yahoo.com>
2022-09-14 12:37:58 +0300

    Adding my projects and console tasks files for Programming Advanced with JS.

---
+++ Programming-advancedd/01. Syntax, Functions and Statement/01. Lab/07. dayOfWeek.js
@@ -0,0 +1,11 @@
+function dayOfWeek(day) {
+    let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    if (days.includes(day)) {
+        console.log(days.indexOf(day) + 1);
+    } else {
+        console.log("error");
+    }
+}
+dayOfWeek('Monday');
+dayOfWeek('Friday');
+dayOfWeek('Invalid');
