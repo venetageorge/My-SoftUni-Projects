967e353ff3903bf2e29b7e430d41f57508265da0
veneta.george <veneta.george@yahoo.com>
2022-09-14 12:33:00 +0300

    Delete Programming-Advanced/01. Syntax, Functions and Statement directory

--- Programming-Advanced/01. Syntax, Functions and Statement/01. Lab/03. largestNumber.js
+++
@@ -1,5 +0,0 @@
-function largestNumber(...params) {
-    console.log(`The largest number is ${Math.max(...params)}.`)
-}
-largestNumber(5, -3, 16);
-largestNumber(-3, -5, -22.5);
--- Programming-Advanced/01. Syntax, Functions and Statement/01. Lab/08. daysInAMonth.js
+++
@@ -1,5 +0,0 @@
-function daysInMonth(month, year) {
-    console.log(new Date(year, month, 0).getDate());
-}
-daysInMonth(1, 2012);
-daysInMonth(2, 2021);
