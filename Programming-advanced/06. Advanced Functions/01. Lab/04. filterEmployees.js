f0e897c3995a5509c42fc27e6b879dfec5034828
venetageorge <veneta.george@yahoo.com>
2022-10-07 12:28:18 +0300

    Adding my projects and console tasks files for Programming Advanced with JS.

---
+++ Programming-advanced/06. Advanced Functions/01. Lab/04. filterEmployees.js
@@ -0,0 +1,33 @@
+function solve(data, criteria) {
+    const [key, value] = criteria.split('-');
+    const persons = JSON.parse(data);
+    const result = persons.filter(obj => obj[key] == value);
+
+    result
+        .filter(obj => obj[key] == value)
+        .map((x, i) => `${i}. ${x.first_name} ${x.last_name} - ${x.email}`)
+        .forEach(el => console.log(el));
+}
+solve(
+    `[{
+    "id": "1",
+    "first_name": "Ardine",
+    "last_name": "Bassam",
+    "email": "abassam0@cnn.com",
+    "gender": "Female"
+  }, {
+    "id": "2",
+    "first_name": "Kizzee",
+    "last_name": "Jost",
+    "email": "kjost1@forbes.com",
+    "gender": "Female"
+  },  
+{
+    "id": "3",
+    "first_name": "Evanne",
+    "last_name": "Maldin",
+    "email": "emaldin2@hostgator.com",
+    "gender": "Male"
+  }]`,
+    'gender-Female'
+);
