043f7aa1598dd19eea3af75bbd4cf31a4d5a5790
venetageorge <veneta.george@yahoo.com>
2022-11-01 11:25:06 +0200

    Adding my projects and console tasks files for Programming Advanced with JS.

---
+++ Programming-advanced/07. Unit Testing and Error Handling/02. Exercise/05. notification.js
@@ -0,0 +1,11 @@
+function notify(message) {
+    let divContent = document.getElementById("notification");
+    divContent.innerText = message; 
+    divContent.style.display = "block";
+    
+    divContent.addEventListener("click", toggleDisplayStyle)
+
+    function toggleDisplayStyle(e) {
+      e.target.style.display = "none";
+    }
+}
