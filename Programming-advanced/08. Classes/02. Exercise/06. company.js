8091e8989e640a3a625fa81cb3cb6ad8743f639f
venetageorge <veneta.george@yahoo.com>
2022-11-05 11:25:04 +0200

    Adding my projects and console tasks files for Programming Advanced with JS.

---
+++ Programming-advanced/08. Classes/02. Exercise/06. company.js
@@ -0,0 +1,53 @@
+class Company {
+    constructor() {
+        this.departments = {};
+    }
+
+    addEmployee(name, salary, position, department) {
+        if (!name || !salary || salary < 0 || !position || !department) {
+            throw new Error('Invalid input!');
+        }
+
+        let employee = {
+            name,
+            salary: Number(salary),
+            position,
+        }
+
+        if (!this.departments[department]) {
+            this.departments[department] = [];
+        }
+        this.departments[department].push(employee);
+        return `New employee is hired. Name: ${name}. Position: ${position}`;
+    }
+
+    bestDepartment() {
+        let bestDepartment = '';
+        let bestAverageSalary = 0;
+
+        for (let dep in this.departments) {
+            let employees = Object.values(this.departments[dep]);
+
+            let averageSalary = 0;
+            employees.forEach((x) => {
+                averageSalary += x['salary'];
+            })
+            averageSalary /= employees.length;
+
+            if (averageSalary > bestAverageSalary) {
+                bestAverageSalary = averageSalary;
+                bestDepartment = dep;
+            }
+        }
+
+        let result = `Best Department is: ${bestDepartment}\nAverage salary: ${bestAverageSalary.toFixed(2)}\n`;
+        
+        let sortedEmployees = Object.values(this.departments[bestDepartment])
+            .sort((a, b) => b.salary - a.salary || a.name.localeCompare(b.name))
+            .map(x => `${x.name} ${x.salary} ${x.position}`);
+        
+            result += sortedEmployees.join('\n');
+
+        return result;
+    }
+}
